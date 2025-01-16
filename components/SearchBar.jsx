e995c227e8dd30a9297f4dea1a854367a2a29c5e
Rohit Banot <94214468+Rbanot78@users.noreply.github.com>
2025-01-16 10:59:49 +0530

    Update SearchBar.jsx

--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -1,6 +1,7 @@
 import { useState, useRef, useEffect } from "react";
 import { useRouter } from "next/router";
 import { useSearch } from "../context/SearchContext";
+import Image from "next/image";
 
 const SearchBar = () => {
   const { searchQuery, setSearchQuery, suggestions, debounceFetchSuggestions } = useSearch();
@@ -104,7 +105,7 @@ const SearchBar = () => {
                   className="flex items-center gap-3 p-3 cursor-pointer hover:bg-blue-100 transition-all duration-200"
                   onClick={() => handleSuggestionClick(product)}
                 >
-                  <img
+               <Image
                     src={product.images[0]}
                     alt={product.title}
                     width={40}
